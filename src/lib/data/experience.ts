0490d733ad858d0594c577a30223a4b782aa2620
Rudra Kumar <rudrakofficial@gmail.com>
2024-03-27 18:06:26 -0400

    updated

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -4,49 +4,49 @@ import { ContractType, type Experience } from '../types';
 
 export const items: Array<Experience> = [
 	{
-		slug: 'open-sourcer',
+		slug: 'web-developer',
 		company: 'Self-employed',
-		description: 'Creating awesome tools for developers.',
+		description: 'Creating custom websites and web applications for clients.',
 		contract: ContractType.SelfEmployed,
-		type: 'Software Development',
-		location: 'Home',
+		type: 'Full Stack Developer',
+		location: 'Remote',
 		period: { from: new Date() },
-		skills: getSkills('ts', 'js'),
-		name: 'Open Source Developer',
-		color: 'red',
-		links: [],
-		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome tools for developers.'
-	},
-	{
-		slug: 'software-freelance',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
-		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date() },
-		skills: getSkills('svelte', 'ts', 'sass', 'css', 'html', 'js'),
-		name: 'Freelancer',
+		skills: getSkills('Opencart', 'Shopify', 'HTML', 'CSS', 'JavaScript', 'PHP', 'MySQL', 'Liquid', 'React', 'Svelte', 'TypeScript'),
+		name: 'Full Stack Developer',
 		color: 'blue',
 		links: [],
 		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome applications for customers.'
+		shortDescription: 'Creating custom websites and web applications for clients.'
 	},
 	{
-		slug: 'software-freelance-junior',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
+		slug: 'aasistant-cloud-engineer',
+		company: 'Infoage Technologies',
+		description: 'Support in managing and optimizing cloud infrastructure to meet organizational requirements.',
 		contract: ContractType.Freelance,
-		type: 'Software Development',
+		type: 'Cloud Engineering',
 		location: 'Home',
-		period: { from: new Date(2022, 0, 1), to: new Date() },
-		skills: getSkills('css', 'html', 'js'),
-		name: 'Junior Freelancer',
+		period: { from: new Date(2020, 1, 1), to: new Date(2022, 11, 1) },
+		skills: getSkills('AWS', 'Azure', 'GCP', 'Networking', 'Linux', 'Scripting'),
+		name: 'Assistant Cloud Engineer',
+		color: 'blue',
+		links: [],
+		logo: Assets.Unknown,
+		shortDescription: 'Support in managing and optimizing cloud infrastructure to meet organizational requirements.'
+	},
+	{
+		slug: 'junior-cloud-engineer',
+		company: 'Infoage Technologies',
+		description: 'Managing and optimizing cloud infrastructure to support business needs.',
+		contract: ContractType.Freelance,
+		type: 'Cloud Engineering',
+		location: 'India',
+		period: { from: new Date(2019, 6, 1), to: new Date(2020, 1, 1) },
+		skills: getSkills('AWS', 'Azure', 'GCP', 'Terraform', 'Docker', 'Kubernetes'),
+		name: 'Junior Cloud Engineer',
 		color: 'green',
 		links: [],
 		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome applications for customers.'
+		shortDescription: 'Managing and optimizing cloud infrastructure to support business needs.'
 	}
 ];
 
