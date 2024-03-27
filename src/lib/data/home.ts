fd7fd82be18c8e1f6539e21a098ef25cb1432e4d
Rudra Kumar <rudrakofficial@gmail.com>
2024-03-27 17:10:26 -0400

    updated

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -11,14 +11,14 @@ export const description: string =
 	'Discover the intersection of innovation and elegance in my portfolio, where each project is a testament to my expertise in Shopify and OpenCart development. Specializing in crafting bespoke eCommerce solutions, I elevate online stores with custom designs, seamless functionality, and optimized performance. My work not only enhances user experience but also drives sales, reflecting a blend of technical proficiency and creative vision. Let\'s transform your eCommerce goals into digital success stories.';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/' },
+	{ platform: Platform.GitHub, link: 'https://github.com/rudrakumarme/' },
 	{
 		platform: Platform.Linkedin,
-		link: 'https://www.linkedin.com/'
+		link: 'https://www.linkedin.com/in/rudrakumarme/'
 	},
 	{
 		platform: Platform.Twitter,
-		link: 'https://twitter.com/'
+		link: 'https://twitter.com/rudrakumarme'
 	},
 	{
 		platform: Platform.StackOverflow,
@@ -26,16 +26,16 @@ export const links: Array<{ platform: Platform; link: string }> = [
 	},
 	{
 		platform: Platform.Email,
-		link: 'riadh_adrani@hotmail.fr'
+		link: 'hello@rudrakumar.me'
 	},
-	{
-		platform: Platform.Youtube,
-		link: 'https://www.youtube.com'
-	},
-	{
-		platform: Platform.Facebook,
-		link: 'https://www.facebook.com'
-	}
+	// {
+	// 	platform: Platform.Youtube,
+	// 	link: 'https://www.youtube.com'
+	// },
+	// {
+	// 	platform: Platform.Facebook,
+	// 	link: 'https://www.facebook.com'
+	// }
 ];
 
 export const skills = getSkills('js', 'css', 'html', 'reactjs', 'sass', 'svelte', 'ts');
