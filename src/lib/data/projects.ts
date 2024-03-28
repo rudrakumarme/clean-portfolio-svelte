1350e537cb8a75063a4ad6f199e905d6dd77582f
Rudra Kumar <rudrakofficial@gmail.com>
2024-03-28 00:59:40 -0400

    updated

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -10,38 +10,18 @@ description:
   "A redesign and abandoned cart extension for Used Products, a European chain with over 60 stores across five countries, specializing in buying, selling, and marketing sustainable products, both new and second-hand, with a focus on providing customers cash for their items and warranty with each purchase. Their unique combination of physical stores and online presence allows them to efficiently serve a diverse set of needs.",
 shortDescription:
   "A redesign and abandoned cart extension for Used Products, a European chain with over 60 stores across five countries, specializing in buying, selling, and marketing sustainable products, both new and second-hand, with a focus on providing customers cash for their items and warranty with each purchase.",
-links: [{ to: 'https://usedproducts.ro/about', label: 'About Used Products' }],
+links: [{ to: 'https://usedproducts.ro/', label: 'Website' }],
 logo: Assets.UsedProducts, // Replace with the appropriate logo variable or link
 name: 'Used Products',
 period: {
-  from: new Date()
+  from: new Date( 2023, 4),
 },
-skills: getSkills('redesign', 'abandoned cart extension'),
+skills: getSkills('opencart', 'php', 'mysql', 'javascript', 'css', 'html'),
 type: 'E-commerce Website',
 		screenshots: [
 			{
 				label: 'screen 1',
-				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '2',
-				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '3',
-				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '4',
-				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '5',
-				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: '6',
-				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+				src: '/static/screenshots/p1_ss.jpg'
 			}
 		]
 	}
