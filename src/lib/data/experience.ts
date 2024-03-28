af63f38596633bdf55fb8f4b3e74efbcf556f650
Rudra Kumar <rudrakofficial@gmail.com>
2024-03-27 20:06:54 -0400

    updated

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -6,7 +6,7 @@ export const items: Array<Experience> = [
 
 	{
 		slug: 'web-developer',
-		company: 'Self-employed',
+		company: 'ClickRays',
 		description: 'Creating custom websites and web applications for clients.',
 		contract: ContractType.SelfEmployed,
 		type: 'Full Stack Developer',
@@ -20,10 +20,10 @@ export const items: Array<Experience> = [
 		shortDescription: 'Creating custom websites and web applications for clients.'
 	},
 	{
-		slug: 'aasistant-cloud-engineer',
+		slug: 'assistant-cloud-engineer',
 		company: 'Infoage Technologies',
 		description: 'Support in managing and optimizing cloud infrastructure to meet organizational requirements.',
-		contract: ContractType.Freelance,
+		contract: ContractType.FullTime,
 		type: 'Cloud Engineering',
 		location: 'India',
 		period: { from: new Date(2020, 1, 1), to: new Date(2022, 11, 1) },
@@ -37,8 +37,8 @@ export const items: Array<Experience> = [
 	{
 		slug: 'junior-cloud-engineer',
 		company: 'Infoage Technologies',
-		description: 'Managing and optimizing cloud infrastructure to support business needs.',
-		contract: ContractType.Freelance,
+		description: 'As a pioneering Junior Cloud Engineer at InfoAge Technologies, India, I spearheaded the integration and management of robust cloud solutions, laying the groundwork for scalable and secure cloud infrastructures. With a keen eye for innovation and a steadfast commitment to excellence, I played a crucial role in automating deployments, enhancing system security, and optimizing resource utilization. My tenure was marked by relentless pursuit of knowledge, collaborative success in cross-functional teams, and a series of strategic achievements that propelled our cloud capabilities to new heights. Leveraging tools like AWS, Python, and CloudWatch, I not only contributed to the creation of a resilient technical ecosystem but also ensured its continuous improvement through proactive monitoring and maintenance. Recognized for my technical acumen and problem-solving prowess, I emerged as a key player in the cloud domain, driving projects to fruition with precision and efficiency. 🌐🔒📈',
+		contract: ContractType.Internship,
 		type: 'Cloud Engineering',
 		location: 'India',
 		period: { from: new Date(2019, 6, 1), to: new Date(2020, 1, 1) },
